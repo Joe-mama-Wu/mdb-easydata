6ea6d52af02cd4d2e4d0c9e22edd677e464987e6
Joe Mama <62436978+Joe-mama-Wu@users.noreply.github.com>
2022-07-19 11:47:56 -0400

    Update easyData-google-analtyics.js

--- Google Analtyics/js/easyData-google-analtyics.js
+++ Google Analtyics/js/easyData-google-analtyics.js
@@ -2,13 +2,14 @@
 
 const GA_SCOPE = "https://www.googleapis.com/auth/analytics.readonly";
 const GA_API_URL = "https://analyticsdata.googleapis.com/$discovery/rest?version=v1beta";
+const GA_PLUGIN_NAME = "quickstart";
 
 gapi.load("client:auth2", function () {
-  gapi.auth2.init({ client_id: CLIENT_ID }).then(() => document.dispatchEvent(new Event('gapi-loaded')));
+  gapi.auth2.init({ client_id: CLIENT_ID,plugin_name: GA_PLUGIN_NAME }).then(() => document.dispatchEvent(new Event('gapi-loaded')));
 });
 
-function signIn(scope = GA_SCOPE) {
-  return gapi.auth2.getAuthInstance().signIn({ scope }).then(() => {
+function signIn(scope = GA_SCOPE,plugin_name= GA_PLUGIN_NAME) {
+  return gapi.auth2.getAuthInstance().signIn({ scope,plugin_name }).then(() => {
     setCookie('guser-loggedin', 'true', 1);
     location.reload();
   }, (e) => console.error(e));
